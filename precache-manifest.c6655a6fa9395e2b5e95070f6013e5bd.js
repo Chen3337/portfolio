f87b93bb824694ba281b5dd46da3d90ef177e25a
zhen yong chen <dannychen111@yahoo.com>
2020-02-22 19:04:44 -0600

    Updates

---
+++ precache-manifest.c6655a6fa9395e2b5e95070f6013e5bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9f23e4072f9ec38d24836f026c7d9f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4401705e0dfdd4f48213",
+    "url": "/portfolio/static/css/main.bf28181e.chunk.css"
+  },
+  {
+    "revision": "6c48b7e318eb57cd0744",
+    "url": "/portfolio/static/js/2.ec8fd688.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.ec8fd688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4401705e0dfdd4f48213",
+    "url": "/portfolio/static/js/main.bc195a59.chunk.js"
+  },
+  {
+    "revision": "dc6c6be44264892063d5",
+    "url": "/portfolio/static/js/runtime-main.2c20904a.js"
+  },
+  {
+    "revision": "8f833a3091bc102936234a4ee4df15b7",
+    "url": "/portfolio/static/media/Resume.8f833a30.pdf"
+  }
+]);
